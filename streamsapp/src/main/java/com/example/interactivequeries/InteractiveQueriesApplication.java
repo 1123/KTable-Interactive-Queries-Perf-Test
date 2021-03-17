3a0df756e5da6bcf7c8950f9f33ea722c4c610e2
Benedikt Linse <benedikt@confluent.io>
2021-03-17 09:39:11 +0100

    splitting apps

---
+++ streamsapp/src/main/java/com/example/interactivequeries/InteractiveQueriesApplication.java
@@ -0,0 +1,36 @@
+package com.example.interactivequeries;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafka
+@EnableKafkaStreams
+public class InteractiveQueriesApplication {
+
+	public final static String PROCESSED_MESSAGES_TOPIC = "processed-messages";
+	public static final String PROCESSED_MESSAGES_STORE_NAME = "processed-messages-store";
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveQueriesApplication.class, args);
+	}
+
+	@Bean
+	public KTable<String, String> kTable(StreamsBuilder kStreamBuilder) {
+		return kStreamBuilder.table(
+				PROCESSED_MESSAGES_TOPIC,
+				Consumed.with(new Serdes.StringSerde(), new Serdes.StringSerde()),
+				Materialized.as(PROCESSED_MESSAGES_STORE_NAME)
+		);
+	}
+
+}
+
---
+++ streamsapp/src/test/java/com/example/interactivequeries/InteractiveQueriesApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.interactivequeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveQueriesApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
