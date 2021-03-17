f4d18d4a5f6d202ac59c138846ebeb6f1ecaab3c
Benedikt Linse <benedikt@confluent.io>
2021-03-17 09:32:38 +0100

    Working on Interactive Queries Example

---
+++ create-input-topic.sh
@@ -0,0 +1,1 @@
+kafka-topics --bootstrap-server localhost:9092 --create --topic processed-messages
---
+++ src/main/java/com/example/interactivequeries/StateStoreQueryService.java
@@ -0,0 +1,27 @@
+package com.example.interactivequeries;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class StateStoreQueryService {
+
+    @Autowired
+    private StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+
+
+    @GetMapping("/store/{id}")
+    public String getSomeKey(@PathVariable String id) {
+        ReadOnlyKeyValueStore<String, String> store =
+                streamsBuilderFactoryBean.getKafkaStreams().store("processed-messages-store", QueryableStoreTypes.keyValueStore());
+        return store.get(id);
+    }
+
+}
