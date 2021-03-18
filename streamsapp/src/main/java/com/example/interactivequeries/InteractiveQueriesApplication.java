15ee51fd02a0dd9297f13d1f8528cdc76fd63f4b
Benedikt Linse <benedikt@confluent.io>
2021-03-18 10:00:57 +0100

    deactivating cleanup of state stores

--- streamsapp/src/main/java/com/example/interactivequeries/InteractiveQueriesApplication.java
+++ streamsapp/src/main/java/com/example/interactivequeries/InteractiveQueriesApplication.java
@@ -3,13 +3,18 @@ package com.example.interactivequeries;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.kafka.core.CleanupConfig;
 
 @SpringBootApplication
 @EnableKafka
@@ -23,12 +28,33 @@ public class InteractiveQueriesApplication {
 		SpringApplication.run(InteractiveQueriesApplication.class, args);
 	}
 
+	/*
+     * We want spring-kafka to NOT clean up the state stores upon startup or shutdown.
+     * Therefore we need to register our own instance of the StreamsBuilderFactoryBean.
+	 */
 	@Bean
-	public KTable<String, Integer> kTable(StreamsBuilder kStreamBuilder) {
+	public StreamsBuilderFactoryBean myKafkaStreamsBuilder(KafkaStreamsConfiguration streamsConfig) {
+		return new StreamsBuilderFactoryBean(streamsConfig, new CleanupConfig(false, false));
+	}
+
+	@Bean
+	public KTable<String, Integer> kTable(@Qualifier("myKafkaStreamsBuilder") StreamsBuilder kStreamBuilder) {
 		return kStreamBuilder.table(
 				PROCESSED_MESSAGES_TOPIC,
 				Consumed.with(new Serdes.StringSerde(), new Serdes.IntegerSerde()),
 				Materialized.as(PROCESSED_MESSAGES_STORE_NAME)
+		);
+	}
+
+	/*
+	 * Dummy stream to avoid "invalid topology exception". Works around the following bug in spring-kafka:
+	 * https://github.com/micronaut-projects/micronaut-kafka/issues/196
+	 */
+	@Bean
+	public KStream<String, Integer> dummyStream(@Qualifier("defaultKafkaStreamsBuilder") StreamsBuilder kStreamBuilder) {
+		return kStreamBuilder.stream(
+				PROCESSED_MESSAGES_TOPIC,
+				Consumed.with(new Serdes.StringSerde(), new Serdes.IntegerSerde())
 		);
 	}
 
--- streamsapp/src/main/java/com/example/interactivequeries/StateStoreQueryService.java
+++ streamsapp/src/main/java/com/example/interactivequeries/StateStoreQueryService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StateStoreQueryService {
 
     @Autowired
+    @Qualifier("myKafkaStreamsBuilder")
     private StreamsBuilderFactoryBean streamsBuilderFactoryBean;
 
     @GetMapping("/store/{id}")
