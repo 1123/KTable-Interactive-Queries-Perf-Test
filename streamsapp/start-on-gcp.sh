1be7c115d3a08c30cafcf39e356eadbc39d1c690
Benedikt Linse <benedikt@confluent.io>
2021-03-28 20:16:05 +0000

    adding metrics exporter

--- streamsapp/start-on-gcp.sh
+++ streamsapp/start-on-gcp.sh
@@ -4,9 +4,9 @@ set -u -e
 
 export SPRING_PROFILES_ACTIVE=$1
 
-curl https://github.com/jeanlouisboudart/kafka-platform-prometheus/blob/master/jmx-exporter/kafka-streams-post-2.5.yml -o /tmp/kafka-streams.yml
+curl https://raw.githubusercontent.com/jeanlouisboudart/kafka-platform-prometheus/master/jmx-exporter/kafka-streams-post-2.5.yml -o /tmp/kafka-streams.yml
 curl https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.15.0/jmx_prometheus_javaagent-0.15.0.jar -o/tmp/jmx_prometheus_javaagent-0.15.0.jar
-export MAVEN_OPTS='-javaagent:/usr/share/jmx_exporter/jmx_prometheus_javaagent-0.12.0.jar=1234:/usr/share/jmx_exporter/kafka-streams-pre-2.5.yml'
+export OPTS='-javaagent:/tmp/jmx_prometheus_javaagent-0.15.0.jar=1234:/tmp/kafka-streams.yml'
 /usr/lib/jvm/java-11-openjdk-11.0.10.0.9-1.el7_9.x86_64/bin/java \
-  -jar /tmp/interactive-queries-streams-app-0.0.1-SNAPSHOT.jar
+  $OPTS -jar /tmp/interactive-queries-streams-app-0.0.1-SNAPSHOT.jar
 
