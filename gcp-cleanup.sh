afae13ec6c666132a99f6e63d61f7c30a0629e41
Benedikt Linse <benedikt@confluent.io>
2021-03-31 08:38:25 +0000

    cleanup

--- gcp-cleanup.sh
+++ gcp-cleanup.sh
@@ -2,18 +2,8 @@
 
 set -e -u
 
-# echo "Stopping local Confluent Kafka"
-# confluent local services stop
-# Not recreating brokers in this environment. 
-
 # echo "Cleaning up any local state"
-# confluent local destroy
-# rm -rf /tmp/kafka-streams/
 # TODO: remove the directory on each of the hosts, where the streams app is running. 
-
-# echo "Starting up Confluent Kafka"
-# confluent local services kafka start
-# not recreating brokers in this environment
 
 . $FUNCTIONS
 . $CPA_ENV
